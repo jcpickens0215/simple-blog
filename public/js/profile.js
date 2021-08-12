dc8ab3efcf7809eb10fa84dfe3245f1189d99e43
Justin Pickens <justin.pickens@gmx.com>
2021-08-11 20:40:13 -0500

    Working POST blog route

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#blog-name').value.trim();
-  const needed_funding = document.querySelector('#blog-funding').value.trim();
-  const description = document.querySelector('#blog-desc').value.trim();
+  const title = document.querySelector('#blog-title').value.trim();
+  const body = document.querySelector('#blog-body').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && body) {
     const response = await fetch('/api/blog', {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, body }),
       headers: {
         'Content-Type': 'application/json',
       },
